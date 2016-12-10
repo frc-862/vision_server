2d26896eecfe0a2a24bb64ce72ed583a97b3b4de
Patrick Hurley <phurley@gmail.com>
2016-12-10 00:20:59 -0500

    Handle feature2d header file correctly

--- src/vision_server.cpp
+++ src/vision_server.cpp
@@ -13,7 +13,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/contrib/contrib.hpp>
 #include <opencv2/core/core.hpp>
-#include <opencv2/features2d.hpp>
+#include <opencv2/features2d/features2d.hpp>
 
 #include "Pipeline.h"
 #include "server.h"
